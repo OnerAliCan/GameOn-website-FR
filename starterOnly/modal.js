bd2512a478d376c5eea81a59af52aeff18c6c60c
Ali <alican.oner.pro@gmail.com>
2024-11-06 17:18:49 +0100

    Form closes on cross click

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -15,9 +15,21 @@ const formData = document.querySelectorAll(".formData");
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
 
+// ******** CODE PERSO ********
+
+//fermer la modale en cliquant sur la croix
+
+const cross = document.querySelector(".close");
+
+cross.addEventListener("click", closeModal);
+
+function closeModal() {
+  modalbg.style.display = "none";
+}
