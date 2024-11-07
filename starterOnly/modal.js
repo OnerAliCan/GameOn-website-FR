65f0167023dcb8e729400b59ddd19108409d82cf
Ali <alican.oner.pro@gmail.com>
2024-11-07 18:53:14 +0100

    verification v1 ok

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,19 +1,17 @@
-window.onload= function() {
+window.onload = function () {
+    // launch modal event
+    modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-document.querySelector(".close").addEventListener("click", closeModal);
-
-}
+    document.querySelector(".close").addEventListener("click", closeModal);
+};
 
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
 
 // DOM Elements
@@ -21,85 +19,89 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
-
-
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+    modalbg.style.display = "block";
 }
-
 
 // ******** CODE PERSO ********
 
 //fermer la modale en cliquant sur la croix
 
-
 function closeModal() {
-  modalbg.style.display = "none";
+    modalbg.style.display = "none";
 }
 
 // verifier le formulaire
 
-const formSubmit = document.querySelector('.btn-submit');
-const firstName = document.getElementById('first');
-const lastName = document.getElementById('last');
+const formSubmit = document.querySelector(".btn-submit");
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
 
 formSubmit.addEventListener("click", verifyForm);
 
 function verifyForm() {
+    // prénom
 
-  // prénom
-
-  if(firstName.value.length <= 2) {
-    alert("Le prénom doit faire plus de 2 caractères");
-    return false;
-  }
-  else {
-    console.log("prenom ok");
-  }
-
-  //nom de famille 
-
-  if(lastName.value.length <= 2) {
-    alert("Le nom de famille doit faire plus de 2 caractères");
-    return false;
-  }
-  else {
-    console.log("nom de famille ok");
-  }
-
-  //Email 
-
-  const emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  const emailInput = document.getElementById('email').value;
-
-  if(emailInput.match(emailPattern)) {
-    console.log('mail ok');
-  }
-  else {
-    alert('format de mail incorrect');
-    return false;
-  }
-
-
-  //nombre de participations
-
-  const quantityInput = document.getElementById('quantity').value;
-    console.log(quantityInput);
-
-    if(!isNaN(quantityInput) && quantityInput != '' ){
-        console.log('participations ok');
+    if (firstName.value.length <= 2) {
+        // alert("Le prénom doit faire plus de 2 caractères");
+        console.log("prénom pas bon");
+        return false;
+    } else {
+        console.log("prenom ok");
     }
-    else {
-        console.log('participations pas bon');
+
+    //nom de famille
+
+    if (lastName.value.length <= 2) {
+        console.log("nom pas bon");
+        // alert("Le nom de famille doit faire plus de 2 caractères");
+        return false;
+    } else {
+        console.log("nom de famille ok");
+    }
+
+    //Email
+
+    const emailPattern =
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    const emailInput = document.getElementById("email").value;
+
+    if (emailInput.match(emailPattern)) {
+        console.log("mail ok");
+    } else {
+        console.log("mail pas bon");
+        // alert('format de mail incorrect');
         return false;
     }
 
+    // date de naissance
 
-  //boutons radios localisation 
+    const dateInput = document.getElementById("birthdate").value;
 
-  var radios = document.getElementsByName('location');
-  let isChecked = false;
+    if (dateInput) {
+        console.log("birthdate ok");
+    } else {
+        console.log("birthdate pas bon");
+        return false;
+    }
+
+    //nombre de participations
+
+    const quantityInput = document.getElementById("quantity").value;
+
+    if (!isNaN(quantityInput) && quantityInput != "") {
+        console.log("nbr participations ok");
+    } else {
+        console.log("nbr participations pas bon");
+        // alert('partipication pas bon');
+        return false;
+    }
+
+    //boutons radios localisation
+
+    var radios = document.getElementsByName("location");
+    let isChecked = false;
 
     for (var i = 0; i < radios.length; i++) {
         if (radios[i].checked) {
@@ -107,19 +109,26 @@ function verifyForm() {
             console.log("Le bouton radio sélectionné est :", radios[i].value);
             break;
         }
-
-        if (!isChecked) {
-            console.log("Aucun bouton radio n'est sélectionné.");
-            
-            return false;
-        }
+    }
+    if (!isChecked) {
+        console.log("Aucun bouton radio n'est sélectionné.");
+        return false;
     }
 
+    // verification checkbox conditions
 
+    var checkbox = document.getElementById("checkbox1");
 
+    if (checkbox.checked) {
+        console.log("Conditions acceptées ok");
+    } else {
+        console.log("conditions non acceptées");
+        // alert('Conditions non acceptées');
+        return false;
+    }
 }
 
-  function validate() {
+function validate() {
     // document.forms["reserve"].reset();
     return false;
-  }
+}
--- test.js
+++ test.js
@@ -1,14 +1,8 @@
-const formSubmit = document.querySelector('.btn-submit');
-formSubmit.addEventListener("click", radioIsChecked);
+const formSubmit = document.querySelector(".btn-submit");
+formSubmit.addEventListener("click", checkboxIsChecked);
 
-// function isNumber(value) {
-//     return typeof value === 'number';
-//   }
-
-
-
-function radioIsChecked() {
-    var radios = document.getElementsByName('location');
+function checkboxIsChecked() {
+    var radios = document.getElementsByName("location");
     let isChecked = false;
 
     for (var i = 0; i < radios.length; i++) {
@@ -17,38 +11,14 @@ function radioIsChecked() {
             console.log("Le bouton radio sélectionné est :", radios[i].value);
             break;
         }
-
-        if (!isChecked) {
-            console.log("Aucun bouton radio n'est sélectionné.");
-            
-            return false;
-        }
+    }
+    if (!isChecked) {
+        console.log("Aucun bouton radio n'est sélectionné.");
+        return false;
     }
 }
-
- 
-
-
-// let output = document.getElementById("output");
-//                       function checkRadio(){
-//                          // accessing the radio buttons
-//                          let radio1 = document.getElementById('radio1');
-//                          let radio2 = document.getElementById('radio2');
-//                          let radio3 = document.getElementById('radio3');
-//                          // checking if any radio button is selected
-//                          if(radio1.checked){
-//                             output.innerHTML = "The radio button with value " + radio1.value + " is checked!";
-//                          }
-//                          if(radio2.checked){
-//                             output.innerHTML = "The radio button with value " + radio2.value + " is checked!";
-//                          }
-//                          if(radio3.checked){
-//                             output.innerHTML = "The radio button with value " + radio3.value + " is checked!";
-//                          }
-//                       }
-
 
 function validate() {
     // document.forms["reserve"].reset();
     return false;
-  }
+}
