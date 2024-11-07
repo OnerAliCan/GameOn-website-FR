344fb1e49de392edf975c94d3ed44d8f0e654b41
Ali <alican.oner.pro@gmail.com>
2024-11-07 12:51:19 +0100

    first w3c check on html & css

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,12 @@
+window.onload= function() {
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+document.querySelector(".close").addEventListener("click", closeModal);
+
+}
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -12,8 +21,6 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 
 // launch modal form
@@ -26,10 +33,46 @@ function launchModal() {
 
 //fermer la modale en cliquant sur la croix
 
-const cross = document.querySelector(".close");
-
-cross.addEventListener("click", closeModal);
 
 function closeModal() {
   modalbg.style.display = "none";
 }
+
+// verifier le formulaire
+
+const formSubmit = document.querySelector('.btn-submit');
+const firstName = document.getElementById('first');
+const lastName = document.getElementById('last');
+
+formSubmit.addEventListener("click", verifyForm);
+
+function verifyForm() {
+
+  if(firstName.value.length <= 2) {
+    alert("Le prénom doit faire plus de 2 caractères");
+  }
+  else {
+    console.log("prenom ok");
+  }
+
+  if(lastName.value.length <= 2) {
+    alert("Le nom de famille doit faire plus de 2 caractères");
+  }
+  else {
+    console.log("nom de famille ok");
+  }
+
+}
+
+function validateEmail(email) {
+
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  
+  return emailPattern.test(email);
+  
+  }
+
+  function validate() {
+    // document.forms["reserve"].reset();
+    return false;
+  }
